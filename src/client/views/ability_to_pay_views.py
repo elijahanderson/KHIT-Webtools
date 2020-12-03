091e2c4d859a4b241703cd3fb97a0206443cf779
Elijah Anderson <elijahanderson_2019@depauw.edu>
2020-12-03 13:19:50 -0700

    Ability to pay (#3)
    * ability-to-pay: Calculator working locally
    
    * ability-to-pay: Small style changes
    
    * ability-to-pay: Fix module imports

---
+++ src/client/views/ability_to_pay_views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request
+
+from client.forms.ability_to_pay_form import AbilityToPayForm
+from infrastructure.ability_to_pay import max_monthly_fee
+
+ability_to_pay_blueprint = Blueprint('ability_to_pay_views', __name__, template_folder='templates')
+
+
+@ability_to_pay_blueprint.route('/ability-to-pay', methods=['GET', 'POST'])
+def ability_to_pay():
+    """ REST endpoint for the ability to pay calculator. """
+    form = AbilityToPayForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            income = form.income.data
+            size = form.size.data
+            fee = 'The maximum monthly fee is $%s' % str(max_monthly_fee(income, size))
+            return render_template('ability_to_pay.html', title='Ability to Pay Calculator', fee=fee, form=form)
+    return render_template('ability_to_pay.html', title='Ability to Pay Calculator', form=form)
