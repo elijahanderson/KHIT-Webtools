0ab99141a471c525ab2f19c514e6a8255d7b3722
Elijah Anderson <elijahanderson_2019@depauw.edu>
2020-12-16 11:30:33 -0700

    Refactor AACOG-Webtools > KHIT-Webtools (#6)
    * refactor-khit-webtools: Begin refactor
    
    * refactor-khit-webtools: Create blank Fremont endpoint
    
    * refactor-khit-webtools: rename VM and small css change

---
+++ src/client/views/aacog_views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+aacog_blueprint = Blueprint('aacog_views', __name__, template_folder='templates')
+
+
+@aacog_blueprint.route('/aacog')
+def aacog():
+    """ REST endpoint for the AACOG Webtools. """
+    tools = {
+        'Ability To Pay Calculator': '/ability-to-pay'
+    }
+    return render_template('aacog.html', title='AACOG Webtools', tools=tools)
--- src/client/views/home_page.py
+++ src/client/views/home_page.py
@@ -6,5 +6,5 @@ home_page = Blueprint('home_page', __name__, template_folder='templates')
 @home_page.route('/', defaults={'page': 'home'})
 @home_page.route('/home', defaults={'page': 'home'})
 def home(page):
-    """ REST endpoint for the home dashboard. """
+    """ REST endpoint for the home page. """
     return render_template('home.html', title='Home')
