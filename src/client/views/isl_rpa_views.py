9803d2eefc36dc78b21c1b3e5bd3bc6139f40bfd
Eli Anderson <dispentia@gmail.com>
2021-02-09 19:04:27 -0700

    isl-rpa-webtool: Automation working locally

--- src/client/views/isl_rpa_views.py
+++ src/client/views/isl_rpa_views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, request
+from traceback import print_exc
 
 from src.client.forms.isl_form import ISLForm
-from src.infrastructure.isl_rpa import isl
+from src.infrastructure.email import send_gmail
+from src.infrastructure.isl_rpa import fremont_isl
 
 
 isl_rpa_blueprint = Blueprint('isl_rpa_views', __name__, template_folder='templates')
@@ -12,6 +14,12 @@ def isl_rpa():
     form = ISLForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            isl(form.from_date.data)
+            try:
+                fremont_isl(form.from_date.data)
+            except Exception as e:
+                print('System encountered an error running Fremont ISL RPA:\n')
+                print_exc()
+                email_body = 'System encountered an error running Fremont ISL RPA: %s' % e
+                send_gmail('eanderson@khitconsulting.com', 'KHIT Report Notification', email_body)
             return render_template('isl_rpa.html', title='ISL Automation', form=form)
     return render_template('isl_rpa.html', title='ISL Automation', form=form)
---
+++ src/infrastructure/drive_upload.py
@@ -0,0 +1,52 @@
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
+import os
+
+
+def service_act_login():
+    """ Log into gmail service account. """
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
+              'https://www.googleapis.com/auth/drive.file',
+              'https://www.googleapis.com/auth/drive',
+              ]
+    SERVICE_ACCOUNT_FILE = 'config/service_key.json'
+
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    service = build('drive', 'v3', credentials=credentials)
+    return service
+
+
+def upload_file(filename, folder_id):
+    print('Uploading file to drive...', end=' ')
+    service = service_act_login()
+    file_metadata = {
+        'name': filename.split('/')[-1],
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(filename, resumable=True)
+    service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print('File uploaded.')
+
+
+def upload_folder(folder_path, parent_folder_id):
+    print('Uploading folder to drive...', end=' ')
+    service = service_act_login()
+    # create folder
+    folder_metadata = {
+        'name': folder_path.split('/')[-1],
+        'parents': [parent_folder_id],
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    drive_folder = service.files().create(body=folder_metadata, fields='id').execute()
+    folder_id = drive_folder.get('id')
+    # upload its contents
+    for file in os.listdir(folder_path):
+        file_metadata = {
+            'name': file,
+            'parents': [folder_id]
+        }
+        media = MediaFileUpload(os.path.join(folder_path, file), resumable=True)
+        service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print('Folder uploaded.')
