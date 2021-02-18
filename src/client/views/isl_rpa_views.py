9154ef5dec5c12d62044d278f093b35d4a0512a9
Elijah Anderson <elijahanderson_2019@depauw.edu>
2021-02-18 15:39:03 -0700

    ISL RPA webtool (#8)
    * isl-rpa-webtool: Initial setup
    
    * isl-rpa-webtool: Automation working locally
    
    * isl-rpa-webtool: Implement loading message and error page
    
    * isl-rpa-webtool: Setup for VM
    
    * isl-rpa-webtool: Having issues getting chromedriver working... Monday task
    
    * master (Fremont RPA): getting to work with docker
    
    * isl-rpa-webtool: Almost finished... just need to tidy up and finalize
    
    * isl-rpa-webtool: Finalize ISL Automation tool
    
    * isl-rpa-webtool: Fix docker chrome crashing error
    
    * isl-rpa-webtool: remove config files

---
+++ src/client/views/isl_rpa_views.py
@@ -0,0 +1,31 @@
+import threading
+
+from flask import Blueprint, redirect, render_template, request
+from traceback import print_exc
+
+from infrastructure.email import send_gmail
+from infrastructure.isl_rpa import fremont_isl
+
+
+isl_rpa_blueprint = Blueprint('isl_rpa_views', __name__, template_folder='templates')
+
+
+@isl_rpa_blueprint.route('/isl-rpa', methods=['GET', 'POST'])
+def isl_rpa():
+    def generate_rpa(**kwargs):
+        try:
+            from_date = kwargs.get('query', {})
+            fremont_isl(query)
+        except Exception as e:
+            print('System encountered an error running Fremont ISL RPA:\n')
+            print_exc()
+            email_body = 'System encountered an error running Fremont ISL RPA: %s' % e
+            send_gmail('eanderson@khitconsulting.com', 'KHIT Report Notification', email_body)
+                                
+    if request.method == 'POST':
+        query = request.form['from-date']
+        thread = threading.Thread(target=generate_rpa, kwargs={'query': query})
+        thread.start()
+        return render_template('isl_rpa.html', title='ISL Automation', loading=True, from_date=query)
+    return render_template('isl_rpa.html', title='ISL Automation')
+
