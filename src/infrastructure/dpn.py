275536a36c8ba1d594deff77c109dc0a88e2e345
Eli <dispentia@gmail.com>
2021-03-09 23:32:33 +0000

    dev: group DPN table rows by program in addition to client

--- src/infrastructure/dpn.py
+++ src/infrastructure/dpn.py
@@ -21,16 +21,17 @@ def modify(df):
         lambda v: str(v).lower().replace('-', '').replace('/', '').replace(' ', '').strip()
     )
     output_dict = {}
-    for client, frame in df.groupby(['full_name']):
-        output_dict[client] = {'id_no': str(frame['id_no'].iloc[0])}
+    for key, frame in df.groupby(['full_name', 'program_name']):
+        dict_key = key[0] + '/' + key[1]
+        output_dict[dict_key] = {'id_no': str(frame['id_no'].iloc[0])}
         for idx, row in frame.iterrows():
             if row['event_name'] in event_categories['event'].unique():
                 category = event_categories.iloc[
                     event_categories.event[event_categories.event == row['event_name']].index
                 ]['category'].iloc[0].strip()
-                if category in output_dict[client].keys():
-                    output_dict[client][category] = str(int(output_dict[client][category]) + 1)
+                if category in output_dict[dict_key].keys():
+                    output_dict[dict_key][category] = str(int(output_dict[dict_key][category]) + 1)
                 else:
-                    output_dict[client][category] = str(1)
+                    output_dict[dict_key][category] = str(1)
     return output_dict
 
