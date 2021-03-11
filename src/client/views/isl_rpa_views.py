2125ed29377b692468d38b8e8a084ba036493945
Eli <dispentia@gmail.com>
2021-03-11 02:56:08 +0000

    dev: begin implementing task status tracking

--- src/client/views/isl_rpa_views.py
+++ src/client/views/isl_rpa_views.py
@@ -1,6 +1,6 @@
 import redis
 
-from flask import Blueprint, current_app, redirect, render_template, request
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
 from rq import Queue, Connection
 
 from infrastructure.email import send_gmail
@@ -17,7 +17,20 @@ def isl_rpa():
         from_date = request.form['from-date']
         with Connection(redis.from_url(current_app.config['REDIS_URL'])):
             q = Queue()
-            task = q.enqueue(fremont_isl, from_date)
-        return render_template('isl_rpa.html', title='ISL Automation', loading=True, from_date=from_date)
+            job = q.enqueue(fremont_isl, from_date)
+            job.meta['from_date'] = from_date
+            job.save_meta()
+        return redirect(url_for('isl_rpa_views.isl_jobs'))
     return render_template('isl_rpa.html', title='ISL Automation')
+
+
+@isl_rpa_blueprint.route('/isl-rpa/jobs', methods=['GET'])
+def isl_jobs():
+    """ REST endpoint for viewing RQ statuses. """
+    with Connection(redis.from_url(current_app.config['REDIS_URL'])):
+        q = Queue()
+        jobs = q.jobs
+        for job in jobs:
+            print(job.get_status())
+        return render_template('jobs.html', title='ISL Automation', jobs=jobs)
 
