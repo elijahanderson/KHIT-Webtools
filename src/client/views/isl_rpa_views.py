72b2be8b28f7e97c52d0577b0208fc57b3673f72
Eli <dispentia@gmail.com>
2021-03-12 08:49:03 +0000

    dev: task status tracking actually working!

--- src/client/views/isl_rpa_views.py
+++ src/client/views/isl_rpa_views.py
@@ -1,3 +1,4 @@
+import json
 import redis
 
 from flask import Blueprint, current_app, redirect, render_template, request, url_for
@@ -18,8 +19,18 @@ def isl_rpa():
         with Connection(redis.from_url(current_app.config['REDIS_URL'])):
             q = Queue()
             job = q.enqueue(fremont_isl, from_date)
-            job.meta['from_date'] = from_date
-            job.save_meta()
+            #job.meta['from_date'] = from_date
+            #job.save_meta()
+            job_id = job.get_id()
+            jobfile = open('json/jobs.json', 'r+')
+            jobs = json.load(jobfile)
+            print(jobs)
+            jobs[job_id] = [from_date]
+            print(jobs)
+            jobfile.close()
+            jobfile = open('json/jobs.json', 'w')
+            json.dump(jobs, jobfile)
+
         return redirect(url_for('isl_rpa_views.isl_jobs'))
     return render_template('isl_rpa.html', title='ISL Automation')
 
@@ -29,8 +40,14 @@ def isl_jobs():
     """ REST endpoint for viewing RQ statuses. """
     with Connection(redis.from_url(current_app.config['REDIS_URL'])):
         q = Queue()
-        jobs = q.jobs
-        for job in jobs:
+        jobfile = open('json/jobs.json', 'r')
+        jobs = json.load(jobfile)
+        print(jobs)
+        for job_id in jobs.keys():
+            print(job_id)
+            job = q.fetch_job(job_id)
+            jobs[job_id].append(job.get_status())
+            jobs[job_id].append(job.result)
             print(job.get_status())
         return render_template('jobs.html', title='ISL Automation', jobs=jobs)
 
